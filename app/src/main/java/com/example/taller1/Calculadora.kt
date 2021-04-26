7bdede34523c941f39aa8c1411c396701de58f0e
Daniel14gonc <71653466+Daniel14gonc@users.noreply.github.com>
2021-04-25 22:49:30 -0600

    Calculo

--- app/src/main/java/com/example/taller1/Calculadora.kt
+++ app/src/main/java/com/example/taller1/Calculadora.kt
@@ -9,6 +9,7 @@ import android.widget.Toast
 class Calculadora : AppCompatActivity() {
 
     lateinit var txtResultado: TextView
+    lateinit var txtRes: TextView
     lateinit var btnCero: Button
     lateinit var btnUno: Button
     lateinit var btnDos: Button
@@ -26,6 +27,8 @@ class Calculadora : AppCompatActivity() {
     lateinit var btnPoint: Button
     lateinit var btnAC: Button
     lateinit var btnEquals: Button
+    lateinit var converter: Converter
+    lateinit var calculadora:Calc
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -49,64 +52,67 @@ class Calculadora : AppCompatActivity() {
         btnPoint = findViewById(R.id.btnPoint)
         btnAC = findViewById(R.id.AC)
         btnEquals = findViewById(R.id.btnEquals)
+        txtRes = findViewById(R.id.res)
+        converter = Converter()
+        calculadora = Calc()
 
         var exp: String = ""
 
         btnCero.setOnClickListener{
-            exp = txtResultado.text.toString();
+            exp = txtResultado.text.toString()
             exp += "0"
             txtResultado.text = exp
         }
 
         btnUno.setOnClickListener{
-            exp = txtResultado.text.toString();
+            exp = txtResultado.text.toString()
             exp += "1"
             txtResultado.text = exp
         }
 
         btnDos.setOnClickListener{
-            exp = txtResultado.text.toString();
+            exp = txtResultado.text.toString()
             exp += "2"
             txtResultado.text = exp
         }
 
         btnTres.setOnClickListener{
-            exp = txtResultado.text.toString();
+            exp = txtResultado.text.toString()
             exp += "3"
             txtResultado.text = exp
         }
         btnCuatro.setOnClickListener{
-            exp = txtResultado.text.toString();
+            exp = txtResultado.text.toString()
             exp += "4"
             txtResultado.text = exp
         }
 
         btnCinco.setOnClickListener{
-            exp = txtResultado.text.toString();
+            exp = txtResultado.text.toString()
             exp += "5"
             txtResultado.text = exp
         }
 
         btnSeis.setOnClickListener{
-            exp = txtResultado.text.toString();
+            exp = txtResultado.text.toString()
             exp += "6"
             txtResultado.text = exp
         }
 
         btnSiete.setOnClickListener{
-            exp = txtResultado.text.toString();
+            exp = txtResultado.text.toString()
             exp += "7"
             txtResultado.text = exp
         }
 
         btnOcho.setOnClickListener{
-            exp = txtResultado.text.toString();
+            exp = txtResultado.text.toString()
             exp += "8"
             txtResultado.text = exp
         }
 
         btnNueve.setOnClickListener{
-            exp = txtResultado.text.toString();
+            exp = txtResultado.text.toString()
             exp += "9"
             txtResultado.text = exp
         }
@@ -114,39 +120,68 @@ class Calculadora : AppCompatActivity() {
 
 
         btnMas.setOnClickListener{
-            exp = txtResultado.text.toString();
-            exp += "+"
-            txtResultado.text = exp
+            exp = txtResultado.text.toString()
+            var exp1 = txtRes.text.toString()
+            if(!(exp.isBlank() and exp.isEmpty())){
+                exp1 += exp + "+"
+                txtRes.text = exp1
+                txtResultado.text=""
+            }
         }
 
         btnMenos.setOnClickListener{
-            exp = txtResultado.text.toString();
-            exp += "-"
-            txtResultado.text = exp
+            exp = txtResultado.text.toString()
+            var exp1 = txtRes.text.toString()
+            if(!(exp.isBlank() and exp.isEmpty())){
+                exp1 += exp + "-"
+                txtRes.text = exp1
+                txtResultado.text=""
+            }
         }
 
         btnMultip.setOnClickListener{
-            exp = txtResultado.text.toString();
-            exp += "x"
-            txtResultado.text = exp
+            exp = txtResultado.text.toString()
+            var exp1 = txtRes.text.toString()
+            if(!(exp.isBlank() and exp.isEmpty())){
+                exp1 += exp + "x"
+                txtRes.text = exp1
+                txtResultado.text=""
+            }
         }
 
         btnDivide.setOnClickListener{
-            exp = txtResultado.text.toString();
-            exp += "÷"
-            txtResultado.text = exp
+            exp = txtResultado.text.toString()
+            var exp1 = txtRes.text.toString()
+            if(!(exp.isBlank() and exp.isEmpty())){
+                exp1 += exp + "÷"
+                txtRes.text = exp1
+                txtResultado.text=""
+            }
         }
 
         btnPoint.setOnClickListener{
-            exp = txtResultado.text.toString();
+            exp = txtResultado.text.toString()
             exp += "."
             txtResultado.text = exp
         }
 
         btnAC.setOnClickListener{
-            exp = txtResultado.text.toString();
-            exp = ""
-            txtResultado.text = exp
+            txtResultado.text = ""
+            txtRes.text = ""
+        }
+
+        btnEquals.setOnClickListener{
+            exp = txtResultado.text.toString()
+            var exp1 = txtRes.text.toString()
+            if(!(exp.isBlank() and exp.isEmpty())){
+                exp1 += exp
+                txtRes.text = exp1
+            }
+            print(exp1)
+            var infixToPostfix = converter.convert(exp1)
+            var temp = calculadora.calcular(infixToPostfix)
+            txtResultado.text = temp
+            txtRes.text = ""
         }
 
     }
